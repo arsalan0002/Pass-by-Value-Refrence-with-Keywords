68726306c2a5bb0745708a56a37a4f6e6fd290cd
Muhammad_Arsalan Iqbal <116029293+arsalan0002@users.noreply.github.com>
2023-02-03 02:18:28 +0500

    Add files via upload
    Practice

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REF_AND_OUT_KEYWORDS
+{
+    class Program
+    {
+        public static void PassByValue(int a)
+        {
+           a = a + 10;
+           Console.WriteLine("Value is: {0}", a);
+        }
+
+//------------------------------------------------------------------------------------------------------------
+
+        public static void PassByRef(ref int a)
+        {
+           a = a + 10;
+           Console.WriteLine("Value is: {0}", a);
+        }
+
+//------------------------------------------------------------------------------------------------------------
+
+        public static void PassByOut(out int a)
+        {
+            a = 20;
+            Console.WriteLine("Value is: {0}", a);
+        }
+
+        static void Main(string[] args)
+        {
+            int value = 5
+            PassByValue(value); // 15
+            Console.WriteLine(value); // 5
+            Console.ReadLine();
+
+//------------------------------------------------------------------------------------------------------------
+
+            int value = 5;
+            PassByRef(ref value); // 15
+            Console.WriteLine(value); // 15
+            Console.ReadLine();
+
+//------------------------------------------------------------------------------------------------------------
+
+            int value;
+            PassByOut(out value); // 20
+            Console.WriteLine(value); // 20
+            Console.ReadLine();
+
+        }
+    }
+}
